667c05901142195e2426a166fc65a15be4299391
ddworak <dawid.dworak@gmail.com>
2015-06-05 12:34:31 +0200

    Initial commit

---
+++ client.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("We who think we are about to die will laugh at anything.\n");
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ninety per cent of most magic merely consists of knowing one extra fact.\n");
+    return 0;
+}
